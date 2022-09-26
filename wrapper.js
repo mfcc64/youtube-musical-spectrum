645a073b26c9137d722a999a4e96ef3ac762f245
Muhammad Faiz <mfcc64@gmail.com>
2022-09-26 15:34:39 +0700

    wrapper.js: remove storage permission

--- wrapper.js
+++ wrapper.js
@@ -1,24 +1,8 @@
 "use strict";
 
 (function() {
-    function run_script() {
-        const script = document.createElement("script");
-        script.type = "module";
-        script.src = chrome.runtime.getURL("script.js");
-        document.head.appendChild(script);
-    }
-
-    const prefix = "__youtube_musical_spectrum_opt_";
-    const opts = ["height", "bar", "waterfall", "brightness", "bass", "speed", "interval", "codecs", "transparent", "visible"];
-    chrome.storage.local.get(opts, (items) => {
-        items = items ? items : {};
-        for (const name in items) {
-            if (!localStorage.getItem(prefix + name)) {
-                try {
-                    localStorage.setItem(prefix + name, Math.round(items[name]));
-                } catch(e) { console.warn(e); }
-            }
-        }
-        chrome.storage.local.clear(() => run_script());
-    });
+    const script = document.createElement("script");
+    script.type = "module";
+    script.src = chrome.runtime.getURL("script.js");
+    document.head.appendChild(script);
 })();
